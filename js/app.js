028115628ed44cf16adf522cd549df18e0d51bca
duner <asduner@gmail.com>
2014-04-25 23:21:16 -0500

    more design updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-$(document)
+$(document).ready(function(){
+
+	$("div.class div.header").click(function() {
+		var parent = $(this).parent();
+		parent.toggleClass("active");
+		var next = $(this).next();
+		next.toggleClass("transition");
+	});
+
+})
